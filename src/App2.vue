abbb0d070665adc7229f3e505dbefe2341790a65
Ayomide9-code <ayotife009@gmail.com>
2025-02-05 12:31:08 -0800

    My first vue project

---
+++ src/App2.vue
@@ -0,0 +1,17 @@
+<!-- <script>
+export default {
+    data() {
+        return {
+            name: 'Ovu Karen',
+            status: false,
+        };
+    },
+};
+</script>
+
+<template>
+        <h1 class="text-pink">{{ name }}</h1>
+        <p v-if="status === 'active'">User is active</p>
+        <p v-else-if="status === 'pending'">User is inactive</p>
+        <p v-else>User is inactive</p>
+</template> -->
